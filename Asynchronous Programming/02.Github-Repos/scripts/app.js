2ac37096ec728d375521e3cb8b7bc692bf5096cc
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2022-07-03 14:30:49 +0300

    async

--- Asynchronous Programming/02.Github-Repos/scripts/app.js
+++ Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -1,22 +1,17 @@
-function loadRepos() {
-	// console.log("TODO...");
+async function loadRepos() {
+	//  console.log("TODO...");
 	const username = document.getElementById('username').value;
 	const list = document.getElementById('repos');
 
-	fetch(`https://api.github.com/users/${username}/repos`)
-	.then(handleResponse)
-	.then(displayRepos)
-	.catch(handleError);
-
-	function handleResponse(response) {
+	try {
+		const response = await fetch(`https://api.github.com/users/${username}/repos`);
+	
 		if (response.ok == false) {
 			throw new Error(`${response.status} ${response.statusText}`)
 		}
-
-		return response.json();
-	}
-
-	function displayRepos(data) {
+		
+		const data = await response.json();
+	
 		list.innerHTML = '';
 		for (let repo of data) {
 			list.innerHTML += 
@@ -27,8 +22,42 @@ function loadRepos() {
 		</li>`;
 		}
 	}
-
-	function handleError(error) {
-		list.innerHTML = `${error.message}`
+	catch (error) {
+		list.innerHTML = `${error.message}`;
 	}
 }
+
+// function loadRepos() {
+// 	// console.log("TODO...");
+// 	const username = document.getElementById('username').value;
+// 	const list = document.getElementById('repos');
+
+// 	fetch(`https://api.github.com/users/${username}/repos`)
+// 	.then(handleResponse)
+// 	.then(displayRepos)
+// 	.catch(handleError);
+
+// 	function handleResponse(response) {
+// 		if (response.ok == false) {
+// 			throw new Error(`${response.status} ${response.statusText}`)
+// 		}
+
+// 		return response.json();
+// 	}
+
+// 	function displayRepos(data) {
+// 		list.innerHTML = '';
+// 		for (let repo of data) {
+// 			list.innerHTML += 
+// 			`<li>
+// 			<a href="${repo.html_url}">
+// 				${repo.full_name}
+// 			</a>
+// 		</li>`;
+// 		}
+// 	}
+
+// 	function handleError(error) {
+// 		list.innerHTML = `${error.message}`
+// 	}
+// }
