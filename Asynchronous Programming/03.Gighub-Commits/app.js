fef23c46fdcb07e803626ce0d364ee9ebf9411cb
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2022-07-03 15:45:24 +0300

    working

--- Asynchronous Programming/03.Gighub-Commits/app.js
+++ Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,26 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;    
+    const list = document.getElementById('commits');
+
+    try {
+        const res = await fetch(`https://api.github.com/repos/${username}/${repo}/commits`);
+    
+        if (res.ok == false) {
+            throw new Error(`${res.status} ${res.statusText}`);
+        }
+
+        const data = await res.json();
+        // const dataAsJson = await res.json();
+        // const data = JSON.parse(dataAsJson);
+        list.innerHTML = '';
+
+        for (let { commit } of data) {
+            list.innerHTML += `<li>${commit.author.name}М ${commit.message}</li>`
+        }
+    }
+    catch (err) {
+        list.innerHTML = `Error: ${err.message}`;
+    }
+
 }
