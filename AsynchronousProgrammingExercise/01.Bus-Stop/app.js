9ce34ecccfdeaba7cb70fa272daa061d5e4ef016
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2022-07-04 07:37:57 +0300

    name

--- AsynchronousProgrammingExercise/01.Bus-Stop/app.js
+++ AsynchronousProgrammingExercise/01.Bus-Stop/app.js
@@ -1,7 +1,22 @@
-function getInfo() {
+async function getInfo() {
     // console.log("TODO...");
 
     const busStopId = document.getElementById('stopId').value;
-    
+    const stopNameDiv = document.getElementById('stopName');
+
+    try {
+        const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${busStopId}`)
+
+        if (response.of == false) {
+
+        }
+        const data = await response.json();
+        console.log(data);
+        stopNameDiv.textContent = data.name;
+        
+    }
+    catch {
+        
+    }
     
 }
