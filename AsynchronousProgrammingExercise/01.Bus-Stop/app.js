27e75ee16bd7c668f9a738439a0f0d948e43b3d4
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2022-07-04 17:10:31 +0300

    Viktor's solution

--- AsynchronousProgrammingExercise/01.Bus-Stop/app.js
+++ AsynchronousProgrammingExercise/01.Bus-Stop/app.js
@@ -1,26 +1,60 @@
 async function getInfo() {
     // console.log("TODO...");
 
-    const busStopId = document.getElementById('stopId').value;
-    const stopNameDiv = document.getElementById('stopName');
-	const list = document.getElementById('buses');
-
+    const stopId = document.getElementById('stopId').value;
+    const stopNameElement = document.getElementById('stopName');
+	const timetableElement = document.getElementById('buses');
+    
+    const url = (`http://localhost:3030/jsonstore/bus/businfo/${stopId}`);
 
     try {
-        const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${busStopId}`)
+        stopNameElement.textContent = `Loading...`;
+        timetableElement.replaceChildren();
 
-        const data = await response.json();
-        console.log(data);
-        stopNameDiv.textContent = data.name;
-        for (let busId in data.buses) {
-            let li = document.createElement('li');
-            li.textContent = `Bus ${busId} arrives in ${data.buses[busId]} minutes`;
-            list.appendChild(li);
-        };
+        const res = await fetch(url);
+        if (res.status != 200) {
+            throw new Error('Stop ID not found');
+        }
+
+        const data = await res.json();
+        stopNameElement.textContent= data.name;
+
+        Object.entries(data.buses).forEach(b => {
+            const liElement = document.createElement('li');
+            liElement.textContent = `Bus ${b[0]} arrives in ${b[1]} minutes`;
+            timetableElement.appendChild(liElement);
+        })
         
     }
     catch {
-        stopNameDiv.textContent = "Error";
+        stopNameElement.textContent = "Error";
     }
     
 }
+
+// async function getInfo() {
+//     // console.log("TODO...");
+
+//     const busStopId = document.getElementById('stopId').value;
+//     const stopNameDiv = document.getElementById('stopName');
+// 	const list = document.getElementById('buses');
+
+
+//     try {
+//         const response = await fetch(`http://localhost:3030/jsonstore/bus/businfo/${busStopId}`)
+
+//         const data = await response.json();
+//         console.log(data);
+//         stopNameDiv.textContent = data.name;
+//         for (let busId in data.buses) {
+//             let li = document.createElement('li');
+//             li.textContent = `Bus ${busId} arrives in ${data.buses[busId]} minutes`;
+//             list.appendChild(li);
+//         };
+        
+//     }
+//     catch {
+//         stopNameDiv.textContent = "Error";
+//     }
+    
+// }
--- AsynchronousProgrammingExercise/02.Bus-Schedule/app.js
+++ AsynchronousProgrammingExercise/02.Bus-Schedule/app.js
@@ -1,12 +1,42 @@
 function solve() {
+    let currentStop = 'depot';
+    const infoDiv = document.getElementById('info');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        // console.log('Depart TODO...');
+        // infoDiv.textContent = `Next stop ${currentStop}`;
+        try {
+            
+            const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStop}`);
+            
+            data = await response.json();
+            infoDiv.textContent = `Next stop ${data.name}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+
+        }
+        catch {
+
+        }
     }
 
-    function arrive() {
-        console.log('Arrive TODO...');
+    async function arrive() {
+        // console.log('Arrive TODO...');
+
+        const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStop}`);
+        data = await response.json();
+
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+        currentStop = data.next;            
+
+        infoDiv.textContent = `Arriving at ${data.name}`;
+
+
     }
+
 
     return {
         depart,
