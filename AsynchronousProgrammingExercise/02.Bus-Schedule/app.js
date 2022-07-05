89ecb93ec89259fe92daf9ae32ee8ea4eb6436f3
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2022-07-05 07:57:00 +0300

    66

--- AsynchronousProgrammingExercise/02.Bus-Schedule/app.js
+++ AsynchronousProgrammingExercise/02.Bus-Schedule/app.js
@@ -1,42 +1,63 @@
 function solve() {
-    let currentStop = 'depot';
-    const infoDiv = document.getElementById('info');
+    let stop = {
+        next: 'depot'
+    }
+    const infoElement = document.getElementById('info');
     const departBtn = document.getElementById('depart');
     const arriveBtn = document.getElementById('arrive');
 
-    async function depart() {
-        // console.log('Depart TODO...');
-        // infoDiv.textContent = `Next stop ${currentStop}`;
-        try {
-            
-            const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStop}`);
-            
-            data = await response.json();
-            infoDiv.textContent = `Next stop ${data.name}`;
-            departBtn.disabled = true;
-            arriveBtn.disabled = false;
 
-        }
-        catch {
-
-        }
+    function depart() {
+        departBtn.disabled = true;
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                stop = JSON.parse(JSON.stringify(data));
+                infoElement.textContent = `Next stop ${stop.name}`;
+            })
+            .catch(error => {
+                infoElement.textContent = "Error"
+            });
+        arriveBtn.disabled = false;
     }
 
-    async function arrive() {
-        // console.log('Arrive TODO...');
-
-        const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${currentStop}`);
-        data = await response.json();
-
-        departBtn.disabled = false;
+    function arrive() {
+        infoElement.textContent = `Arriving at ${stop.name}`;
         arriveBtn.disabled = true;
-        currentStop = data.next;            
-
-        infoDiv.textContent = `Arriving at ${data.name}`;
-
+        departBtn.disabled = false;
 
     }
 
+    // async function depart() {
+    //     // console.log('Depart TODO...');
+    //     // infoDiv.textContent = `Next stop ${currentStop}`;
+    //     // try {
+            
+    //         departBtn.disabled = true;
+    //         const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${stop.next}`);
+    //         // if (response.status != 200) {
+    //         //     throw new Error('Invalid data');
+    //         // }
+    //         stop = await response.json();
+
+    //         infoDiv.textContent = `Next stop ${stop.name}`;
+    //         arriveBtn.disabled = false;
+    //     // }
+    //     // catch {
+    //     //     infoDiv.textContent = `Error`;
+    //     //     departBtn.disabled = true;
+    //     //     arriveBtn.disabled = true;
+    //     // }
+    // }
+
+    // function arrive() {
+    //     // console.log('Arrive TODO...');
+
+    //     infoDiv.textContent = `Arriving at ${stop.name}`;
+    //     departBtn.disabled = false;
+    //     arriveBtn.disabled = true;
+    // }
 
     return {
         depart,
