f8e46fa5515195e398ca355e9b1c4819d90ee5ff
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2022-07-05 15:39:12 +0300

    100

--- AsynchronousProgrammingExercise/03.Forecaster/app.js
+++ AsynchronousProgrammingExercise/03.Forecaster/app.js
@@ -13,15 +13,15 @@ function attachEvents() {
     let rain = "&#x2614";
     let degrees = "&#176";
     let code = '';
-    let divElementUpcoming = document.createComment('div');
-    let divElementCurrent = document.createComment('div');
+    let divElementUpcoming = document.createElement('div');
+    let divElementCurrent = document.createElement('div');
 
     getButton.addEventListener('click', (e) => {
         divElementUpcoming.innerHTML = "";
         divElementCurrent.innerHTML = "";
 
         divElementUpcoming.setAttribute('class', 'forecast-info');
-        divElementCurrent.setAttribute('class', 'forecast');
+        divElementCurrent.setAttribute('class', 'forecasts');
 
         divDisplay.style.display = "inline";
 
@@ -43,9 +43,14 @@ function attachEvents() {
                         let locationSpan = document.createElement('span');
                         let spanWithIcon = document.createElement('span');
 
+                        spanWithIcon.setAttribute('class', 'condition symbol');
+                        spanGroup.setAttribute('class', 'condition');
+                        locationSpan.setAttribute('class', 'forecast-data');
+                        temperatureSpan.setAttribute('class', 'forecast-data');
+                        conditionSpan.setAttribute('class', 'forecast-data');
+
                         locationSpan.textContent = data.name;
-                        temperatureSpan.innerHTML = `${data.forecast.low}${degrees}/
-                            ${data.forecast.high}${degrees}`;
+                        temperatureSpan.innerHTML = `${data.forecast.low}${degrees}/${data.forecast.high}${degrees}`;
                         conditionSpan.textContent = data.forecast.condition;
                         let condition = data.forecast.condition;
                         if(condition == 'Sunny') {
@@ -70,6 +75,51 @@ function attachEvents() {
                         currentDiv.appendChild(divElementCurrent)
                     })
                     .catch(error => console.log(error))
+
+                fetch(`${baseUrl}/upcoming/${code}`)
+                    .then(response => response.json())
+                    .then(data => {
+
+                        let nextDays = data.forecast;
+
+                        nextDays.forEach(x => {
+                            
+                            let spanGroup = document.createElement('span');
+                            let conditionSpan = document.createElement('span');
+                            let temperatureSpan = document.createElement('span');
+                            // let locationSpan = document.createElement('span');
+                            let spanWithIcon = document.createElement('span');
+    
+                            spanWithIcon.setAttribute('class', 'symbol');
+                            spanGroup.setAttribute('class', 'upcoming');
+                            temperatureSpan.setAttribute('class', 'forecast-data');
+                            conditionSpan.setAttribute('class', 'forecast-data');
+                            
+                            // locationSpan.textContent = x.name;
+                            temperatureSpan.innerHTML = `${x.low}${degrees}/${x.high}${degrees}`;
+                            conditionSpan.textContent = x.condition;
+                            let condition = x.condition;
+
+                            if(condition == 'Sunny') {
+                                spanWithIcon.innerHTML = sunny;
+                            }
+                            else if (condition == 'Partly sunny') {
+                                spanWithIcon.innerHTML = partlySunny
+                            }
+                            else if (condition == 'Overcast') {
+                                spanWithIcon.innerHTML = overcast
+                            }
+                            else if (condition == 'Rain') {
+                                spanWithIcon.innerHTML = rain
+                            }
+    
+                            spanGroup.appendChild(spanWithIcon);
+                            spanGroup.appendChild(temperatureSpan);
+                            spanGroup.appendChild(conditionSpan);
+                            divElementUpcoming.appendChild(spanGroup)
+                            upcomingDiv.appendChild(divElementUpcoming);
+                        })
+                    })
             })
 
     })
