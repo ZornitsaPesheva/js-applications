ae01e9b45ebd2b4b6a06cfa19b88325a8ba78c41
zornitsaserbezova <zornitsa.serbezova@balkangraph.com>
2022-07-19 12:54:02 +0300

    01

--- 14. Exercise Client Side Rendering/01.List-Towns/app.js
+++ 14. Exercise Client Side Rendering/01.List-Towns/app.js
@@ -1,1 +1,16 @@
-import { html, render } from ""
+import { html, render } from "./node_modules/lit-html/lit-html.js"
+
+document.getElementById('btnLoadTowns').addEventListener('click', (e) => {
+    e.preventDefault();
+    let input = document.querySelector('#towns').value;
+
+    let townList = input.split(', ');
+    let cardTemplate = html `
+    <ul>
+        ${townList.map(t => html `<li>${t}</li>`)}
+    </ul>
+    `;
+
+    let root = document.getElementById('root')
+    render(cardTemplate, root)
+})
